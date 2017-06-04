99c314347dc663f1bc08ad02244a1ff79180683d
Pavel <sur-pavel@mail.ru>
2017-06-04 06:38:18 +0300

    improve Viewer

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,7 +6,6 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -30,22 +29,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends Application {
-    private TableView<SearchValue> searchTableView = new TableView<>();
+
     private final ObservableList<SearchValue> titles = FXCollections.observableArrayList();
     private List<DataField> dataFields;
     private ArrayList<Record> records = new ArrayList<>();
+    private ArrayList<GuiField> guiFieldArrayList = new ArrayList<>();
     private Record currentRecord;
-    //    private ObservableList<SearchValue> searchValues = FXCollections.observableArrayList();
-    private ArrayList<GuiField> guiFields;
-    private GridPane editorGridPane;
-    private MarcReader reader;
+    private TabPane editorPane = new TabPane();
     private WebEngine webEngine;
+    private StringBuilder builder;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         unimarcGet();
+        createGuiFields();
         primaryStage.setMaximized(true);
+//        primaryStage.setFullScreen(true);
         currentRecord = records.get(0);
 
 // viewBorderPane
@@ -53,10 +53,6 @@ public class Main extends Application {
         webView.autosize();
         webView.setPrefSize(950, 220);
         webEngine = webView.getEngine();
-        StringBuilder builder = new StringBuilder();
-        dataFields = currentRecord.getDataFields();
-        viewerBuilder(builder);
-        webEngine.loadContent(builder.toString());
         BorderPane viewBorderPane = new BorderPane();
         viewBorderPane.setCenter(webView);
         viewBorderPane.setPadding(new Insets(10));
@@ -82,70 +78,13 @@ public class Main extends Application {
         foundGPane.setPadding(new Insets(10));
         foundGPane.add(foundTable, 1, 1);
 
-// editorTabPane
-        // editorGridPane
-        guiFields = new ArrayList<>();
-        //      List fields = records.get(1).getVariableFields(tags);
-        dataFields = currentRecord.getDataFields();
-        for (DataField dataField : dataFields) {
-            guiFields.add(new GuiField(dataField));
-        }
-        editorGridPane = new GridPane();
-        //        gridPane.setAlignment(Pos.BASELINE_CENTER);
-        editorGridPane.setVgap(5);
-        editorGridPane.setHgap(5);
-        editorGridPane.setPadding(new Insets(10));
-        editorGridPane.setAlignment(Pos.CENTER);
-        for (int i = 0, size = guiFields.size(); i < size; i++) {
-            editorGridPane.add(new CheckBox(), 0, i + 1);
-
-            Label label = new Label(guiFields.get(i).getName());
-            label.setMinWidth(200);
-            editorGridPane.add(label, 1, i + 1);
-
-            editorGridPane.add(new Button(String.valueOf(guiFields.get(i).getButtonNum())), 2, i + 1);
-            TextField field = new TextField(guiFields.get(i).getValue());
-            field.setPrefWidth(980);
-            editorGridPane.add(field, 3, i + 1);
-        }
-        //tabTech
-        Tab tabTech = new Tab("Технология");
-        ScrollPane scrollPane1 = new ScrollPane();
-        scrollPane1.setFitToHeight(true);
-        ScrollBar scrollBar1 = new ScrollBar();
-        scrollBar1.setMax(500);
-        scrollBar1.setMin(0);
-        scrollBar1.setValue(100);
-        scrollBar1.setUnitIncrement(30);
-        scrollBar1.setBlockIncrement(35);
-//        String[] tags1 = {"010", "100", "245", "250", "260", "300"};
-        scrollPane1.setContent(editorGridPane);
-        tabTech.setContent(scrollPane1);
-
-/*        // tab Экземпляры
-        Tab tab2 = new Tab("Экземпляры");
-        ScrollPane scrollPane2 = new ScrollPane();
-        scrollPane2.setFitToHeight(true);
-        ScrollBar scrollBar2 = new ScrollBar();
-        scrollBar2.setMax(500);
-        scrollBar2.setMin(0);
-        scrollBar2.setValue(100);
-        scrollBar2.setUnitIncrement(30);
-        scrollBar2.setBlockIncrement(35);
-//        String[] tags2 = {"010", "100", "245", "250", "260", "300"};
-        scrollPane2.setContent(editorGridPane);
-        tabTech.setContent(scrollPane2);*/
-
-
-        TabPane editorTabPane = new TabPane();
-        editorTabPane.getTabs().add(tabTech);
-//        editorTabPane.getTabs().add(tab2);
-        // tabS
-
-
+// editorPane
+        setEditorPane();
+        setViewer();
 //searchVBox
 
         //searchTableView
+        TableView<SearchValue> searchTableView = new TableView<>();
         searchTableView.setEditable(true);
         TableColumn countValueColumn = new TableColumn("Кол-во");
         countValueColumn.prefWidthProperty().bind(searchTableView.widthProperty().multiply(0.2));
@@ -157,9 +96,14 @@ public class Main extends Application {
         searchValueColumn.prefWidthProperty().bind(searchTableView.widthProperty().multiply(0.8));
         searchValueColumn.setCellValueFactory(
                 new PropertyValueFactory<>("searchValue"));
+
         searchTableView.setItems(titles);
         searchTableView.getColumns().add(countValueColumn);
         searchTableView.getColumns().add(searchValueColumn);
+        searchValueColumn.setSortType(TableColumn.SortType.ASCENDING);
+        searchTableView.getSortOrder().add(searchValueColumn);
+        searchValueColumn.setSortable(true);
+        searchTableView.sort();
 
         VBox searchVBox = new VBox();
         HBox hBox = new HBox();
@@ -174,7 +118,7 @@ public class Main extends Application {
 
 
 //rootSplitPane
-        SplitPane splitPaneH1 = new SplitPane(searchVBox, editorTabPane);
+        SplitPane splitPaneH1 = new SplitPane(searchVBox, editorPane);
         splitPaneH1.setDividerPositions(0.2);
         splitPaneH1.setOrientation(Orientation.HORIZONTAL);
         SplitPane splitPaneH2 = new SplitPane(foundGPane, viewBorderPane);
@@ -183,45 +127,101 @@ public class Main extends Application {
         SplitPane splitPaneV = new SplitPane(splitPaneH1, splitPaneH2);
         splitPaneV.setOrientation(Orientation.VERTICAL);
         searchTableView.getSelectionModel().getSelectedItems().addListener(onItemSelected);
-
         Scene scene = new Scene(splitPaneV);
         primaryStage.setTitle("Library Client");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    private void setEditorPane() {
+
+        int[] tags = {200, 210, 10, 700};
+        editorPane.getTabs().clear();
+        editorPane.getTabs().add(getТab("Основное БО", tags));
+        int[] tags2 = {910};
+        editorPane.getTabs().add(getТab("Экземпляры", tags2));
+    }
+
+    private Tab getТab(String name, int[] tags) {
+        Tab tab = new Tab(name);
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setFitToHeight(true);
+        ScrollBar scrollBar = new ScrollBar();
+        scrollBar.setMax(500);
+        scrollBar.setMin(0);
+        scrollBar.setValue(100);
+        scrollBar.setUnitIncrement(30);
+        scrollBar.setBlockIncrement(35);
+        VBox vbox = new VBox();
+        for (GuiField guiField : guiFieldArrayList) {
+            for (int tag : tags) {
+                if (tag == guiField.getFieldNumber()) {
+                    vbox.getChildren().add(guiField.getHBox());
+                }
+            }
+        }
+        scrollPane.setContent(vbox);
+        tab.setContent(scrollPane);
+
+        dataFields = currentRecord.getDataFields();
+        for (DataField dataField : dataFields) {
+            for (GuiField guiField : guiFieldArrayList) {
+                if (Integer.parseInt(dataField.getTag()) == guiField.getFieldNumber()) {
+                    builder = new StringBuilder();
+                    setStringBuilder(dataField);
+                    guiField.setValue(builder.toString());
+
+                }
+            }
+        }
+        return tab;
+    }
+
+    private void setStringBuilder(DataField dataField) {
+        List subFields = dataField.getSubfields();
+        for (Object subField : subFields) {
+            Subfield subfield = (Subfield) subField;
+            char code = subfield.getCode();
+            String data = subfield.getData();
+            builder.append("$").append(code).append(data);
+        }
+    }
+
+    private StringBuilder getViewerBuilder() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("<b>")
+                .append(getData(675, 'a',"<br>"))
+                .append(getData(899, 'm',"<br>"))
+                .append(getData(700, 'a',", "))
+                .append(getData(700, 'g',"<br>"))
+                .append("</b>")
+                .append(getData(200, 'a'," / "))
+                .append(getData(700, 'b'," "))
+                .append(getData(700, 'a',". - "))
+                .append(getData(210, 'a'," : "))
+                .append(getData(210, 'c',", "))
+                .append(getData(210, 'd',"."))
+
+
+        ;
+        return builder;
+    }
+
+    private void setViewer() {
+        webEngine.loadContent(getViewerBuilder().toString());
+    }
+
     private ListChangeListener<SearchValue> onItemSelected = itemSelected -> {
         for (SearchValue searchValue : itemSelected.getList())
-
             for (Record record : records) {
                 List recordFields = record.find("200", searchValue.getSearchValue());
                 for (Object recordField : recordFields) {
                     String foundedString = recordField.toString();
                     if (foundedString.contains(searchValue.getSearchValue())) {
-                        guiFields = new ArrayList<>();
-                        dataFields = record.getDataFields();
-                        for (DataField dataField : dataFields) {
-                            guiFields.add(new GuiField(dataField));
-                        }
-                        editorGridPane.getChildren().removeAll();
-                        for (int i = 0, size = guiFields.size(); i < size; i++) {
-                            editorGridPane.add(new CheckBox(), 0, i + 1);
-
-                            Label label = new Label(guiFields.get(i).getName());
-                            label.setMinWidth(200);
-                            editorGridPane.add(label, 1, i + 1);
-
-                            editorGridPane.add(new Button(String.valueOf(guiFields.get(i).getButtonNum())), 2, i + 1);
-                            TextField newField = new TextField(guiFields.get(i).getValue());
-                            newField.setPrefWidth(980);
-                            editorGridPane.add(newField, 3, i + 1);
-
-
-                            StringBuilder builder = new StringBuilder();
-                            dataFields = record.getDataFields();
-                            viewerBuilder(builder);
-                            webEngine.loadContent(builder.toString());
-                        }
+                        currentRecord = record;
+                        setEditorPane();
+                        setViewer();
 
                     }
                 }
@@ -229,24 +229,10 @@ public class Main extends Application {
 
     };
 
-    private void viewerBuilder(StringBuilder builder) {
-        for (DataField dataField : dataFields) {
-            String tag = dataField.getTag();
-            builder.append(tag);
-            List subFields = dataField.getSubfields();
-            for (Object subField : subFields) {
-                Subfield subfield = (Subfield) subField;
-                char code = subfield.getCode();
-                String data = subfield.getData();
-                builder.append("$").append(code).append(data);
-            }
-            builder.append("<br>");
-        }
-    }
 
     private void unimarcGet() throws FileNotFoundException {
         InputStream in = new FileInputStream("ManyRecords.ISO");
-        reader = new MarcStreamReader(in, "UTF8");
+        MarcReader reader = new MarcStreamReader(in, "UTF8");
 
         // fill records
         while (reader.hasNext()) {
@@ -270,6 +256,50 @@ public class Main extends Application {
 
     }
 
+    private void createGuiFields() {
+        for (String s : Constanst.fieldsName) {
+            int num = Integer.parseInt(s.split(":")[0]);
+            guiFieldArrayList.add(new GuiField(num, s));
+        }
+    }
+
+    private String getFieldData(int number, char code) {
+        StringBuilder builder = new StringBuilder();
+        for (DataField dataField : dataFields) {
+            int tag = Integer.parseInt(dataField.getTag());
+            if (tag == number) {
+                List subFields = dataField.getSubfields(code);
+                for (Object subField : subFields) {
+                    Subfield subfield = (Subfield) subField;
+                    builder.append(subfield.getData());
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    private String getData(int number, char code, String nextString){
+        StringBuilder builder = new StringBuilder();
+        String string = getFieldData(number, code);
+        if (!string.equals("")){
+            builder.append(string)
+                    .append(nextString);
+        }
+        return builder.toString();
+    }
+
+    private String getFieldData(int number, char code, int subFNum) {
+        return getString(number, code, subFNum - 1);
+    }
+
+    private String getString(int number, char code, int subFNum) {
+        String data = "";
+        DataField field = (DataField) currentRecord.getVariableField(String.valueOf(number));
+        List subfields = field.getSubfields(code);
+        Subfield subfield = (Subfield) subfields.get(subFNum);
+        data = subfield.getData();
+        return data;
+    }
 
     public static void main(String[] args) {
         launch(args);
